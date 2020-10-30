b8ea3f57072eda5df1d052a76640f9a53cdef159
khatruong2009 <khatruong2009@gmail.com>
2020-10-30 14:56:53 -0400

    started the project

---
+++ random-color/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Color from "./Components/Color";
+
+function App() {
+  return (
+    <main>
+      <Color />
+    </main>
+  );
+}
+
+export default App;
---
+++ random-color/src/Components/Color.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Color = () => {
+  return (
+    <div>
+      <h1>Random Color Generator</h1>
+      <button>New Color</button>
+    </div>
+  )
+}
+
+export default Color;
